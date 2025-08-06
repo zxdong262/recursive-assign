935b69bd5e44070e6ab39c76983a2dc3cdbafbcf
Msfv3n0m <jlbieker32@gmail.com>
2025-08-06 11:17:22 -0400

    indent logic

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ function recExtend(obj1, obj2) {
   var ks = _.keys(obj2)
   _.each(ks, function(key) {
     if (key !== '__proto__' && key !== 'constructor' && key !== 'prototype') {
-    var res1 = obj1[key]
-    var res2 = obj2[key]
-    if (_.isPlainObject(res1) && _.isPlainObject(res2)) {
-      recExtend(obj1[key], res2)
-    } else if (_.isFunction(res2)) {
-      obj1[key] = res2(res1)
-    } else {
-      obj1[key] = res2
+      var res1 = obj1[key]
+      var res2 = obj2[key]
+      if (_.isPlainObject(res1) && _.isPlainObject(res2)) {
+        recExtend(obj1[key], res2)
+      } else if (_.isFunction(res2)) {
+        obj1[key] = res2(res1)
+      } else {
+        obj1[key] = res2
+      }
     }
-  }
   })
   return obj1
 }
